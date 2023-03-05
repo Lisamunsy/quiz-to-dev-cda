e6846b45cc4fd31c79495152bf7616b620b59924
Lisa Munsy <stillailleurs@gmail.com>
2023-03-05 10:46:35 +0100

    Init project repository

---
+++ qtd-api/src/main/java/com/moon/qtdapi/QtdApiApplication.java
@@ -0,0 +1,13 @@
+package com.moon.qtdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QtdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QtdApiApplication.class, args);
+	}
+
+}
---
+++ qtd-api/src/test/java/com/moon/qtdapi/QtdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon.qtdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QtdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
